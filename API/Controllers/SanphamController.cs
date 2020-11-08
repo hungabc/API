81debc59fdbf63341fd5b55cf3fa1f6112360c54
hungabc <70497683+hungabc@users.noreply.github.com>
2020-11-08 23:08:41 +0700

    8/11/2020

--- API/Controllers/SanphamController.cs
+++ API/Controllers/SanphamController.cs
@@ -47,7 +47,7 @@ namespace API.Controllers
                 var arrData = model.HINH.Split(';');
                 if (arrData.Length == 3)
                 {
-                    var savePath = $@"assets/images/hinhanh/Products/{arrData[0]}";
+                    var savePath = $@"{arrData[0]}";
                     model.HINH = $"{savePath}";
                     SaveFileFromBase64String(savePath, arrData[2]);
                 }
